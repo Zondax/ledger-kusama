d2fe1176db5f15005cd4ccbe5be423393a976ea2
Carlo Sala <carlosalag@protonmail.com>
2022-04-25 15:13:23 +0200

    small fixes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ledger Kusama App (eXpeCT ChAoS)
+# Ledger Kusama App
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 [![GithubActions](https://github.com/zondax/ledger-kusama/actions/workflows/main.yml/badge.svg)](https://github.com/Zondax/ledger-kusama/blob/main/.github/workflows/main.yaml)
--- app/src/substrate/substrate_types_V11.c
+++ app/src/substrate/substrate_types_V11.c
@@ -1,18 +1,18 @@
 /*******************************************************************************
-*  (c) 2019 - 2022 Zondax GmbH
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-********************************************************************************/
+ *  (c) 2019 - 2022 Zondax GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
 #include "bignum.h"
 #include "coin.h"
 #include "parser_impl.h"
@@ -260,8 +260,10 @@ parser_error_t _readLookupasStaticLookupSource_V11(parser_context_t* c, pd_Looku
         break;
     case 3: // Address32
         GEN_DEF_READARRAY(32)
+        break;
     case 4: // Address20
         GEN_DEF_READARRAY(20)
+        break;
     default:
         return parser_unexpected_value;
     }
@@ -1164,7 +1166,7 @@ parser_error_t _toStringLookupasStaticLookupSource_V11(
         GEN_DEF_TOSTRING_ARRAY(20)
     }
     default:
-        return parser_unexpected_address_type;
+        return parser_not_supported;
     }
 
     return parser_ok;
